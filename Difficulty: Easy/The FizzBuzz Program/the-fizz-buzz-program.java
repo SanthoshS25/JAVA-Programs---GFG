499f2d341327c5f444ed8a95b6d8f7d4f8bd9270
Santhosh S <santhoshnarayanan2004@gmail.com>
2025-03-20 22:18:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/The FizzBuzz Program/the-fizz-buzz-program.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.Scanner;
+
+class Main {
+
+    
+// } Driver Code Ends
+//User function Template for Java
+
+  public static void fizzBuzz(int number) {
+        if (number > 0) {
+            if (number % 3 == 0 && number % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (number % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (number % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(number);
+            }
+        } else {
+            System.out.println("Invalid input: Enter a positive number.");
+        }
+    }
+
+    // if (number % 3 ==0){
+    //     System.out.println("Fizz");
+        
+        
+        
+    // }
+    // if (number % 5 ==0){
+    //     System.out.println("Buzz");
+    // }
+    // if(number % 3 == 0 && number %5 ==0){
+    //     System.out.println("FizzBuzz");
+    // }
+    // if (number !=0){
+    //     System.out.println(number);
+        
+    // }
+    
+
+
+//{ Driver Code Starts.
+
+    public static void main(String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+        while(t-- > 0) {
+            int number = scn.nextInt();
+            fizzBuzz(number);
+        
+System.out.println("~");
+}
+        scn.close();
+    }
+}
+// } Driver Code Ends
