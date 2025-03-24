e163186ee83f27b9dd56854aa2a2ee5801fc3e8c
Santhosh S <santhoshnarayanan2004@gmail.com>
2025-03-24 20:37:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Input String/input-string.java
@@ -0,0 +1,19 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        // Take String Input and Print it
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        System.out.print(str);
+    }
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
