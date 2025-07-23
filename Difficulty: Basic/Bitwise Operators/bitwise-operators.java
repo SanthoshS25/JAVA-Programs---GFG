cfacb1059ca4730d9f0ebac99f3510df45314264
Santhosh S <santhoshnarayanan2004@gmail.com>
2025-07-23 21:02:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Bitwise Operators/bitwise-operators.java
+++ Difficulty: Basic/Bitwise Operators/bitwise-operators.java
@@ -1,46 +1,18 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
+//Back-end complete function Template for Java
 import java.util.Scanner;
 
-class Main {
-
-    
-// } Driver Code Ends
-
-// User function Template for Java
-
-public static void operators(int a, int b, int c) {
-    // Do a^a below
-    int d = a^a;
-        // Do c^b below
-        int e =c^b;
-            // Do a&b below
-        int f =a&b;
-            // Do c|(a^a) below
-        int g =c|(d);
-            // Do ~e below
-        e = ~e;
-
-            // The line below prints the output. Don't change it!
-        System.out.println(d + " " + e + " " + f + " " + g);
-}
-
-
-//{ Driver Code Starts.
-
-    public static void main(String[] args) {
+public class Solution {
+    public static void solve() {
         Scanner scn = new Scanner(System.in);
-        int t = scn.nextInt();
-        while (t-- > 0) {
-            int a = scn.nextInt();
-            int b = scn.nextInt();
-            int c = scn.nextInt();
-            operators(a, b, c);
-
-            System.out.println("~");
-        }
-        scn.close();
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        int c = scn.nextInt();
+        int d = a^a;
+        int e = c^b;
+        int f = a&b;
+        int g = c|(a^a);
+        e = ~e;
+        System.out.print(d+" "+e+" "+f+" "+g);
+        // Perform all the operations and print in a single line
     }
 }
-// } Driver Code Ends
