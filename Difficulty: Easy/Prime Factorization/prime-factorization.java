91cf06eca916033ecc3a27ccf4b19261eef1a5a9
Santhosh S <santhoshnarayanan2004@gmail.com>
2025-07-26 19:42:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Prime Factorization/prime-factorization.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static void printPrimeFactorization(int n) {
+        // code here
+        int i=2;
+        while(i<=n){
+            if(n%i==0){
+                System.out.print(i+" ");
+                n/=i;
+            }
+            else{
+                i++;
+            }
+        }
+    }
+}
