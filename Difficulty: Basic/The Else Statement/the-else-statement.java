15e8dad9ba39985d870c3dffdf1c415341281a53
Santhosh S <santhoshnarayanan2004@gmail.com>
2025-07-30 20:33:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/The Else Statement/the-else-statement.java
+++ Difficulty: Basic/The Else Statement/the-else-statement.java
@@ -1,41 +1,15 @@
-//{ Driver Code Starts
-//Initial Template for Java
+import java.util.*;
 
-import java.util.Scanner;
-
-class Main {
-
-    
-// } Driver Code Ends
-//User function Template for Java
-
-public static void utility(int number){
-    //Write your if-else statement below
-    if (number > 100){
-        System.out.println("Big");
-    }
-    else {
-        System.out.println("Number");
-        
-    }
-    
-    
-    //Write your if-else statement above
-}
-
-//{ Driver Code Starts.
-
-    public static void main(String[] args)
-    {
-        Scanner scn = new Scanner(System.in);
-        int t = scn.nextInt();
-        while(t-- > 0) {
-            int number = scn.nextInt();
-            utility(number);
-        
-System.out.println("~");
-}
-        scn.close();
+public class Solution {
+    public static void main(String args[]) {
+        // Your Code Here
+        Scanner s = new Scanner(System.in);
+        int n =s.nextInt();
+        if(n>100){
+            System.out.print("Big");
+        }
+        else{
+            System.out.print("Number");
+        }
     }
 }
-// } Driver Code Ends
