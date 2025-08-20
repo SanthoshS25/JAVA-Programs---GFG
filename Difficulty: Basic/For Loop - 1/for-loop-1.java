eeb16d5dd3c64186f5dada3372f5e9c4b6f5b0f5
Santhosh S <santhoshnarayanan2004@gmail.com>
2025-08-20 20:09:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/For Loop - 1/for-loop-1.java
+++ Difficulty: Basic/For Loop - 1/for-loop-1.java
@@ -1,39 +1,14 @@
-//{ Driver Code Starts
-//Initial Template for Java
+import java.util.*;
 
-import java.util.Scanner;
-
-class Main {
-
-    
-// } Driver Code Ends
-
-//User function Template for Java
-
-public static void utility(int n){
-    
-    //code here
-    //for loop to be added
-    for(int i=1;i<=10;i++){
-        System.out.print(n*i+" ");
+public class Solution {
+    public static void solve() {
+        // Your Code Here
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i = 1;
+        while(i<=10){
+            System.out.print(n*i+" ");
+            i++;
+        }
     }
 }
-
-
-//{ Driver Code Starts.
-
-    public static void main(String[] args)
-    {
-        Scanner scn = new Scanner(System.in);
-        int t = scn.nextInt();
-        while(t-- > 0) {
-            int n = scn.nextInt();
-            utility(n);
-            System.out.println();
-        
-System.out.println("~");
-}
-        scn.close();
-    }
-}
-// } Driver Code Ends
